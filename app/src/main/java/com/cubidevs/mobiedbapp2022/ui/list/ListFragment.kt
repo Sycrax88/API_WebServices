4ce60cd7e681bd39d3a9e3360a5274b96cd19684
Sebastian Ospina Garcia <sebastian47121@gmail.com>
2022-09-04 15:01:18 -0500

    Initial commit

---
+++ app/src/main/java/com/cubidevs/mobiedbapp2022/ui/list/ListFragment.kt
@@ -0,0 +1,52 @@
+package com.cubidevs.mobiedbapp2022.ui.list
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.cubidevs.mobiedbapp2022.databinding.FragmentListBinding
+import com.cubidevs.mobiedbapp2022.server.model.Movie
+
+class ListFragment : Fragment() {
+
+    private lateinit var listBinding: FragmentListBinding
+    private lateinit var listViewModel: ListViewModel
+    private var moviesList: ArrayList<Movie> = ArrayList()
+    private lateinit var moviesAdapter: MoviesAdapter
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        listViewModel = ViewModelProvider(this)[ListViewModel::class.java]
+        listBinding = FragmentListBinding.inflate(inflater, container, false)
+        val root: View = listBinding.root
+
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        moviesAdapter = MoviesAdapter(moviesList, onItemClicked = { })
+
+        listBinding.moviesRecyclerView.apply {
+            layoutManager = LinearLayoutManager(this@ListFragment.requireContext())
+            adapter = moviesAdapter
+            setHasFixedSize(false)
+        }
+
+        listViewModel.moviesLoaded.observe(viewLifecycleOwner){ result ->
+            onMoviesLoadedSubscribe(result)
+        }
+
+        listViewModel.getMovies()
+    }
+
+    private fun onMoviesLoadedSubscribe(moviesList: ArrayList<Movie>?) {
+        moviesList?.let { moviesList ->
+            moviesAdapter.appendItems(moviesList)
+        }
+    }
+}
